2124c9b5422017f32de2f0847ccd253bfa56f892
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-25 11:26:51 +0300

    fixing mobile input

--- pages/components/Product.js
+++ pages/components/Product.js
@@ -91,24 +91,16 @@ export default function Product({
         {/* Images upload */}
         <div className="flex flex-col gap-4">
           <div className="flex items-center">
-            <label
-              className="text-lg font-medium text-gray-700 mr-2 fileInputLabel"
-              onClick={() => document.getElementById('fileInput').click()}
-            >
-              Images
-            </label>
+            <label className="text-lg font-medium text-gray-700 mr-2">Images</label>
             <div className="flex items-center justify-center rounded-lg">
-              <label
-                htmlFor="fileInput"
-                className="flex items-center gap-1.5 px-3 py-2 text-center text-sm font-medium text-gray-500 border cursor-pointer hover:border-primary-400"
-              >
+              <label htmlFor="fileInput" className="flex items-center gap-1.5 px-3 py-2 text-center text-sm font-medium text-gray-500 border cursor-pointer hover:border-primary-400">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-4 w-4">
                   <path d="M9.25 13.25a.75.75 0 001.5 0V4.636l2.955 3.129a.75.75 0 001.09-1.03l-4.25-4.5a.75.75 0 00-1.09 0l-4.25 4.5a.75.75 0 101.09 1.03L9.25 4.636v8.614z" />
                   <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
                 </svg>
                 Upload
               </label>
-              <input id="fileInput" type="file" className="hidden" onChange={uploadImages} />
+              <input id="fileInput" type="file" className="hidden" accept="image/*" capture="camera" multiple onChange={uploadImages} />
             </div>
           </div>
 
