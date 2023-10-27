f532520e5ad345e4e8c344a1b0b8f0e80be49b33
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-27 21:15:24 +0300

    updating button

--- pages/categories.js
+++ pages/categories.js
@@ -9,7 +9,7 @@ export default function Categories() {
   const [parentCategory, setParentCategory] = useState('');
   const [editedCategory, setEditedCategory] = useState(null);
 
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const closeModal = () => {
     setShowModal(false);
   };
@@ -98,10 +98,13 @@ export default function Categories() {
                 <input type="text" id="example11" class="block w-[400px] rounded-md border border-slate-300 py-2.5 pl-8 pr-16 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="Category Name"
                   value={name}
                   onChange={ev => setName(ev.target.value)}
+                  required
                 />
               </div>
             </div>
-            <button type="submit" class="rounded-lg border border-blue-100 bg-blue-100 px-5 py-3 text-center text-sm font-medium text-blue-600 transition-all hover:border-blue-200 hover:bg-blue-200 focus:ring focus:ring-blue-50 disabled:border-blue-50 disabled:bg-blue-50 disabled:text-blue-400">Save</button>
+            <button type="submit" class="rounded-lg border border-blue-100 bg-blue-100 px-5 py-3 text-center text-sm font-medium text-blue-600 transition-all hover:border-blue-200 hover:bg-blue-200 focus:ring focus:ring-blue-50 disabled:border-blue-50 disabled:bg-blue-50 disabled:text-blue-400">
+              {editedCategory ? 'Save changes' : 'Save Category'}
+            </button>
           </form>
         </div>
         <hr className="my-8 h-px border-0 bg-gray-300" />
