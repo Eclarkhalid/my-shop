cd1392d5e8a501c604410c2dab028114e91ea6d3
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-27 21:03:15 +0300

    adding delete

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -16,4 +16,17 @@ export default async function handle(req, res) {
   if (method === 'GET') {
     res.json(await Category.find().populate('parent'))
   }
+
+  if(method === 'PUT') {
+    const {name, parentCategory, _id} = req.body;
+
+    const categoryDoc = await Category.updateOne({_id}, {name, parent: parentCategory || undefined});
+    res.json(categoryDoc)
+  }
+
+  if (method === 'DELETE') {
+    const {_id} = req.query;
+    await Category.deleteOne({_id});
+    res.json('ok');
+  }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -9,6 +9,15 @@ export default function Categories() {
   const [parentCategory, setParentCategory] = useState('');
   const [editedCategory, setEditedCategory] = useState(null);
 
+  const [showModal, setShowModal] = useState(true);
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -21,8 +30,16 @@ export default function Categories() {
 
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post('/api/categories', { name, parentCategory });
+    const data = { name, parentCategory }
+    if (editedCategory) {
+      data._id = editedCategory._id
+      await axios.put('/api/categories', data);
+      setEditedCategory(null)
+    } else {
+      await axios.post('/api/categories', data);
+    }
     setName('')
+    setParentCategory('')
     fetchCategories();
   }
 
@@ -30,6 +47,13 @@ export default function Categories() {
     setEditedCategory(category);
     setName(category.name);
     setParentCategory(category.parent?._id)
+  }
+
+  async function deleteCategory(category) {
+    const {_id} = category;
+    await axios.delete('/api/categories?_id=' +_id);
+    closeModal();
+    fetchCategories();
   }
 
   return <>
@@ -116,17 +140,51 @@ export default function Categories() {
                 >
                   Edit
                 </Link>
-                <Link
-                  href={''}
-                  className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
+                <div
+                  onKeyDown={() => setShowModal(false)}
+                  className="inline-block rounded bg-red-600 text-xs font-medium text-white hover:bg-red-700"
                 >
-                  Delete
-                </Link>
+                  <button onClick={toggleModal} className="px-4 py-2">
+                    Delete
+                  </button>
+                  {showModal && (
+                    <>
+                      <div className="fixed inset-0 z-10 bg-gray-700/50"></div>
+                      <div className="fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-0">
+                        <div className="mx-auto w-full overflow-hidden rounded-lg bg-white shadow-xl sm:max-w-sm">
+                          <div className="relative p-5">
+                            <div className="text-center">
+                              <div className="mx-auto mb-5 flex h-10 w-10 items-center justify-center rounded-full bg-red-100 text-red-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
+                              </div>
+                              <div>
+                                <h3 className="text-lg font-medium text-gray-900">Delete blog post</h3>
+                                <div className="mt-2 text-sm text-gray-500 max-w-sm">Are you sure you want to delete this {category?.name}?</div>
+                              </div>
+                            </div>
+                            <div  className="mt-5 flex justify-end gap-3">
+                              <button onClick={closeModal} className="flex-1 rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-100 focus:ring focus:ring-gray-100 disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-400">Cancel</button>
+                              <button className="flex-1 rounded-lg border border-red-500 bg-red-500 px-4 py-2 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-red-700 hover:bg-red-700 focus:ring focus:ring-red-200 disabled:cursor-not-allowed disabled:border-red-300 disabled:bg-red-300"
+                                onClick={() => deleteCategory(category)}
+                              >Delete</button>
+                            </d>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
               </td>
             </tr>
           </tbody>
         ))}
       </table>
+
+
     </div>
+
+
   </>
 }
