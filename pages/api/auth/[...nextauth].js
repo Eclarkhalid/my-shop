6d1684f9f5bdce0394aaf46f5c06bd263acd9e79
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-29 19:52:23 +0300

    fixing auth errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,9 @@
 import clientPromise from '@/lib/mongodb'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import NextAuth, { getServerSession } from 'next-auth'
+import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-const adminEmails = ['muirurirc@gmail.com']
-
-export const authOptions = {
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -14,24 +12,4 @@ export const authOptions = {
   ],
   adapter: MongoDBAdapter(clientPromise),
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    session: ({session, token, user}) => {
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    }
-  }
-}
-
-export default NextAuth(authOptions)
-
-export async function isAdminRequest(req,res) {
-  const session = getServerSession(req,res,authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end;
-    throw 'You are not allowed to access this page'
-  }
-}
+})
