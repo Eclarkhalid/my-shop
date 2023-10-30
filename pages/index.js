6c14022969120ae064cd94785ce1d1acb1bff6bd
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-30 19:27:10 +0300

    updating backend

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,17 @@ export default function Home() {
                     />
                   </svg>
                 </Link>
+                <Link href={'https://my-shop-front-eclarkhalid.vercel.app/'} target="_blank"
+                  className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-orange-500 px-5 py-3 text-orange-500 transition hover:bg-orange-50 hover:text-orange-700 focus:outline-none focus:ring"
+                  type="button"
+                >
+                  <span className="text-sm font-medium"> View Shop </span>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                  </svg>
+
+                </Link>
                 {/* <button onClick={handleLogout}
                   className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-red-400 px-5 py-3 text-red-500 transition hover:bg-red-50 hover:text-red-700 focus:outline-none focus:ring"
                   type="button"
