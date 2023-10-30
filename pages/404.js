05c3ddf110c214b929b66576a09172d83c5893b2
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-30 09:03:05 +0300

    updating 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,7 @@ export default function ErrorPage() {
           Uh-oh!
         </p>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">We can&apos;t find that page.</p>
 
         <a
           href="/"
--- pages/settings/index.js
+++ pages/settings/index.js
@@ -16,7 +16,7 @@ export default function Settings() {
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
             <p className="mt-1 text-md leading-6 text-gray-600">
-              This information will be displayed publicly so be careful what you share.
+              This information will only be displayed to you.
             </p>
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
