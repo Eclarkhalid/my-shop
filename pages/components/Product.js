2d24aa9fca0492af3aed60419c99db643e771141
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-30 13:32:58 +0300

    adding a delet function to images

--- pages/components/Product.js
+++ pages/components/Product.js
@@ -21,7 +21,7 @@ export default function Product({
   const [isUploading, setIsUploading] = useState(false);
   const uploadImagesQueue = [];
   const [categories, setCategories] = useState([]);
-  const [category, setCategory] = useState( selectedCategory || '');
+  const [category, setCategory] = useState(selectedCategory || '');
 
   useEffect(() => {
     axios.get('/api/categories').then(result => {
@@ -85,7 +85,13 @@ export default function Product({
     setImages(images)
   }
 
- 
+  function handleDeleteImage(index) {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+  }
+
+
 
   return (
     <div className="mx-auto max-w-2xl">
@@ -149,12 +155,19 @@ export default function Product({
           {!isUploading && (
             <div className=" grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mb-2">
               <ReactSortable list={images} setList={updateImagesOrder} className="w-[350px] h-auto  gap-2 flex  justify-between align-items-center">
-                {!!images?.length &&
-                  images.map((link) => (
-                    <div key={link} className="flex-1 border rounded bg-gray-200">
-                      <img src={link} alt="image" className="object-cover h-32 w-44" />
+                {images?.map((link, index) => (
+                  <div key={link} className="relative group">
+                    <img src={link} alt="image" className="object-cover h-32 w-44 rounded-md border p-2 cursor-pointer transition-transform transform-gpu group-hover:scale-105" />
+                    <div className="absolute top-2 right-2 cursor-pointer opacity-0 group-hover:opacity-100">
+                      <button onClick={() => handleDeleteImage(index)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-orange-600 bg-white rounded-full">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                      </button>
                     </div>
-                  ))}
+                  </div>
+                ))}
+
               </ReactSortable>
             </div>
           )}
